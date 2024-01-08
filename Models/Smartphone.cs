0aed9ed8a035e5889d588e8f7e1af1437f290501
PatriciaNFS <148078612+PatriciaNFS@users.noreply.github.com>
2024-01-08 20:38:54 -0300

    Add files via upload

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,25 +2,49 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        public string Numero {get; set;}
+        private string Modelo {get; }
+        private string Imei {get; }
+        private int Memoria {get; }
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
-
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
         }
-
         public void ReceberLigacao()
         {
+            Console.Clear();
             Console.WriteLine("Recebendo ligação...");
-        }
+            Console.WriteLine("Spam...");
+            Thread.Sleep(500);
+            Console.WriteLine("Ligação perigosa");
+            Thread.Sleep(800);
+            Console.WriteLine("\nAtender ligação?(S/N)");
+            string opcao = Console.ReadLine();
 
+            if (opcao == "S" || opcao == "s") 
+            {
+              Console.WriteLine("Oi, a TIM tem uma nova oferta para você!...");
+              
+            }
+            else if (opcao == "N" || opcao == "n")
+            {
+                Console.WriteLine("Chamada recusada...");
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Opção inválida, tente novamente.");
+                ReceberLigacao();
+            }
+
+        }
         public abstract void InstalarAplicativo(string nomeApp);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,66 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+                           
+Console.Clear();
+Console.WriteLine("Escolha o tipo de Smartphone: ");
+Console.WriteLine("1- Nokia");
+Console.WriteLine("2- Iphone");                    
+int tipoSmartphone = Int32.Parse(Console.ReadLine());
+                    
+Smartphone smartphone = null; 
+switch (tipoSmartphone)
+    {
+        case 1:
+            smartphone = new Nokia ("(81) 998575899", "Nokia C2", "111222333444555", 32);
+            break;
+        case 2:
+            smartphone = new Iphone("(81) 998778144", "Iphone 10", "555444333222111", 64);
+            break;
+        default:
+            Console.Clear();
+            Console.WriteLine("Opção inválida, encerrando o programa.");                        
+            break;
+    }
+        
+
+bool encerrar = false;
+
+while (!encerrar)
+{
+    Console.Clear();
+    Console.WriteLine("Menu do Smartphone:");
+    Console.WriteLine("1- Receber ligação");
+    Console.WriteLine("2- Efetuar ligação");
+    Console.WriteLine("3- Instalar aplicativo");
+    Console.WriteLine("4- Encerrar");
+
+    int opcao = Int32.Parse(Console.ReadLine());
+
+    switch (opcao)
+    {
+        case 1: 
+            smartphone.ReceberLigacao();
+        break;
+        case 2:
+            smartphone.Ligar();
+        break;
+        case 3:
+            Console.Write("Qual aplicativo você deseja instalar? ");
+            string nomeApp = Console.ReadLine();
+            smartphone.InstalarAplicativo(nomeApp);
+        break;
+        case 4:
+            encerrar = true;
+            Console.WriteLine("Desligando o Smartphone.");
+        break;
+
+        default:
+            Console.WriteLine("Opção inválida.");
+        break;
+    } 
+    Console.WriteLine("Pressione Enter para continuar!");
+    Console.ReadLine();
+
+}
+
